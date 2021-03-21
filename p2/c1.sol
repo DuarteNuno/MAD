c0bc8d7b5cac32d9a4582a2d5ada5a66003012cb
Duarte Alves <71823585+DuarteNuno@users.noreply.github.com>
2021-03-21 19:21:56 +0000

    Add files via upload

---
+++ p2/c1.sol
@@ -0,0 +1,53 @@
+Problem:    c1
+Rows:       12
+Columns:    9
+Non-zeros:  30
+Status:     OPTIMAL
+Objective:  lucro = 47300 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 R1           NU           300                         300            25 
+     2 R2           NU           200                         200           120 
+     3 R3           NU            20                          20            75 
+     4 R4           NU            40                          40            65 
+     5 R5           NU            60                          60            55 
+     6 R6           NU            80                          80            70 
+     7 R7           NU            60                          60            40 
+     8 R8           B             40                          70 
+     9 R9           NU            20                          20            20 
+    10 R10          B              0                          20 
+    11 R11          B              0                          30 
+    12 lucro        B          47300                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 opomadN      B             20             0               
+     2 opomadD      B             40             0               
+     3 opomadP      B             60             0               
+     4 opoparN      B             80             0               
+     5 opoparD      B             60             0               
+     6 opoparP      B             40             0               
+     7 madparN      B             20             0               
+     8 madparD      B              0             0               
+     9 madparP      NL             0             0                         -20 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
---
+++ p2/ex2.sol
@@ -0,0 +1,38 @@
+Problem:    ex2
+Rows:       4
+Columns:    2
+Non-zeros:  7
+Status:     OPTIMAL
+Objective:  lucro = 370 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 R1           B            5.8                           7 
+     2 R2           NU            40                          40            10 
+     3 R3           NL            30            30                          -1 
+     4 lucro        B            370                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 xM           B              3             0               
+     2 xT           B            2.8             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
